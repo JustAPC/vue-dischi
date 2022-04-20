6b38a12cd166c84e5205ff2da5d935a32dea6d8a
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-20 17:07:49 +0200

    prove soluzione bonus

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loading-screen">Stiamo caricando la tua musica!</div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  color: white;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container">
-    <div class="row py-4">
-      <SongCard v-for="(element, i) in songList" :key="i" :poster="element.poster" :title="element.title" :author="element.author" :year="element.year" />
+  <div>
+    <div class="loading-screen"><LoadingScreen /></div>
+    <div class="container">
+      <div class="row py-4">
+        <SongCard v-for="(element, i) in songList" :key="i" :poster="element.poster" :title="element.title" :author="element.author" :year="element.year" />
+      </div>
     </div>
   </div>
 </template>
@@ -9,12 +12,14 @@
 <script>
 import axios from "axios";
 import SongCard from "./SongCard.vue";
+import LoadingScreen from "./LoadingScreen.vue";
 
 export default {
   name: "MainComp",
   props: {},
   components: {
     SongCard,
+    LoadingScreen,
   },
   data() {
     return {
@@ -22,6 +27,8 @@ export default {
     };
   },
   created() {
+    this.loadScreen();
+
     axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((res) => {
@@ -31,6 +38,17 @@ export default {
       .catch((error) => {
         console.log(error);
       });
+  },
+
+  methods: {
+    loadScreen: function () {
+      let loadingScreen = document.getElementsByClassName("loading-screen");
+      if (this.songList.length != this.res.data.response.length) {
+        loadingScreen.style.display = "block";
+      } else {
+        loadingScreen.style.display = "none";
+      }
+    },
   },
 };
 </script>
